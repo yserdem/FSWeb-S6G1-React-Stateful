662335214908341128d71ff1bb0d19eb06e8796c
Sinan <y.sinanerdem@gmail.com>
2023-03-27 19:05:27 +0300

    Döndürücü tamamlandı?

--- src/components/Dondurucu.js
+++ src/components/Dondurucu.js
@@ -36,22 +36,23 @@ ADIM 4:
 */
 
 import React from 'react'; /* STEP 0 */
+import { useState } from 'react';
 
 export default function Dondurucu() {
-/* ADIM 1 */
+  const [döndürücüAçık, setdöndürücüAcik] = useState(true);
 
   const toggleDondurucu = () => {
-  /* ADIM 4 */
+    setdöndürücüAcik(!döndürücüAçık);
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Döndürücü</h2>
       {
-        true && <div id='döndürücü' className='spinner'>--+--</div> /* ADIM 2 */
+       döndürücüAçık && <div id='döndürücü' className='spinner'>--+--</div> /* ADIM 2 */
       }
       <button id='toggleDondurucu' onClick={toggleDondurucu}>
-         Gizle {/* STEP 3 */}
+        {döndürücüAçık ? 'Gizle' : 'Göster'}
       </button>
     </div>
   );
