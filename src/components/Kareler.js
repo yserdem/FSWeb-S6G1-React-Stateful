247e916669d4e50bb366ad7808a7c0fdbad1660e
Sinan <y.sinanerdem@gmail.com>
2023-03-27 21:11:16 +0300

    kareler tamamlandı

--- src/components/Kareler.js
+++ src/components/Kareler.js
@@ -15,11 +15,10 @@ Aşaıdaki yorumları takip edin.
 */
 
 import React from 'react';
-
+import { useState } from 'react';
 
 //Bu değişkeni YALNIZCA bir durum dilimini yüklemek için kullanın!
 const KareIdListesi = ['sqA', 'sqB', 'sqC', 'sqD'];
-
 
 
 export default function Kareler() {
@@ -27,19 +26,22 @@ export default function Kareler() {
   // 'aktifKare' olmak üzere. Birisi kare idlerini _dizi_ olarak tutacak, diğeri ise aktif olan
   // kareyi gözlemleyecek. Sayfa yüklendiğinde aktif kare olmayacak,
   // yani  'aktifKare' null olmalı.
+  const [kareler, setKareler] = useState(KareIdListesi);
+  const [aktifKare, setAktifKare] = useState(null);
 	
   const ClassAdiAl = id => {
     // Bu bir click handler değildir, JSX içinde kullanılan bir yardımcıdır(helper).(aşağıya bakın)
     // Eğer argüman olarak verilen id aktif kare state'indeki id ile eşleşirse, class adı 'active' olan bir string döndürecek
     // diğer durumlar için boş döndürecek.
     // Etkisini görmek için kareye sağ tıklayın ve "öğeyi inceleyin".
-	return ''
+	return aktifKare === id ? 'active' : ''
   };
 
   const AktifEt = id => {
     // Bu bir _satır içinden çağırılmış_ click handler yardımcısıdır.
     // id bağımsız değişkenini, stateteki aktif id olacak şekilde ayarlayın
     // eğer zaten aktifse, o zaman önce state i resetlemeliyiz.
+    setAktifKare(id === aktifKare ? null : id)
   };
 
   return (
